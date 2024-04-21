5ffcdd43f048f685272d4112532ce51bf0b734c0
whoami13579 <liangforcoding@gmail.com>
2024-04-21 13:37:54 +0800

    create simple database

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_required
+from .models import Role, User
+from . import db
+from datetime import datetime
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+@views.route("/home")
+def home():
+    return "Home Page"
